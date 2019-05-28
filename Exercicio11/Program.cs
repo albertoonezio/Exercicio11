d135f48c23ba8768445fd033bc58ecc89f9fff46
Alberto <albertomaronezio@hotmai.com>
2019-05-28 17:01:52 -0300

    Exercicio11

---
+++ Exercicio11/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n = 0;
+            double resultado = 0;
+            Console.Write("Digite um Número: ");
+            n = double.Parse(Console.ReadLine());
+            resultado = n / 3;
+            Console.WriteLine("A terça parte deste número é: {0:#.00}", resultado);
+        }
+    }
+}
